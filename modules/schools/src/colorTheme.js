eeaa1fa1743d305012862333e673241f60cc9937
Shruti Agarwal <mail2shruti.ag@gmail.com>
2022-07-12 13:30:26 +0530

    fix: calendar component
    Signed-off-by: Shruti Agarwal <mail2shruti.ag@gmail.com>

--- modules/schools/src/colorTheme.js
+++ modules/schools/src/colorTheme.js
@@ -1,14 +1,32 @@
 const colorTheme = {
   primary: "#6461D2",
+  warning: "#FFC369",
+  danger: "#F57B7B",
+  gray: "#9E9E9E",
+  coolGray: "#e5e7eb",
+  lightGray: "#d1d5db",
+  grayInLight: "#9ca3af",
+  darkGray: "#4b5563",
+  grayInDark: "#374151",
+  presentCardCompareText: "#FA8129",
+  weekCardCompareBg: "#FFF8F7",
+  reportBoxBg: "#FFF8F7",
+  attendancePresent: "#2BB639",
+  attendanceAbsent: "#F57B7B",
+  attendanceUnmarked: "#B5B5C8",
+  attendancedefault: "#d3d3e5",
+  date: "#333333",
+  dateText: "#9C9EA0",
+  specialDuty: "#6461D2",
+  holiDay: "#F0F0F4",
+  isActive: "#545454",
 };
 
-export const visitedCard = {
-  cardBg: "#ECF7EB",
-  visitedCardBorderColor: "#C5DCC3",
-  shadow: "0px 5px 40px rgba(0, 0, 0, 0.05)",
-  muted: "#666666",
-};
-
-export const colors = {};
+// export const visitedCard = {
+//   cardBg: "#ECF7EB",
+//   visitedCardBorderColor: "#C5DCC3",
+//   shadow: "0px 5px 40px rgba(0, 0, 0, 0.05)",
+//   muted: "#666666",
+// };
 
 export default colorTheme;
--- modules/schools/src/pages/TeacherAttendanceReport.js
+++ modules/schools/src/pages/TeacherAttendanceReport.js
@@ -1,24 +1,28 @@
-import React from "react";
-import { Layout } from "@shiksha/common-lib";
+import moment from "moment";
 import {
+  Actionsheet,
   Box,
-  Center,
-  VStack,
-  Text,
-  HStack,
-  Avatar,
-  Divider,
-  Spacer,
-  Pressable,
   Button,
+  HStack,
+  Pressable,
+  Stack,
+  Text,
+  VStack,
+  Divider,
 } from "native-base";
+import React, { useState, useEffect } from "react";
+import { TouchableHighlight } from "react-native-web";
 import {
-  DEFAULT_THEME,
-  H2,
   IconByName,
-  Collapsible,
+  Layout,
   ProgressBar,
+  H2,
+  Collapsible,
+  overrideColorTheme,
+  calendar,
 } from "@shiksha/common-lib";
+import colorTheme from "colorTheme";
+const colors = overrideColorTheme(colorTheme);
 
 const TeacherAttendanceReport = () => {
   const [progressData, setProgressData] = React.useState([
@@ -38,6 +42,26 @@ const TeacherAttendanceReport = () => {
       value: 1,
     },
   ]);
+  const [weekPage, setWeekPage] = useState(0);
+  const [attendanceType, setAttendanceType] = useState("MORNING_SCHOOL");
+  const [teacherObject, setTeacherObject] = useState({});
+  const teacherId = localStorage.getItem("id");
+  const token = localStorage.getItem("token");
+  const [attendance, setAttendance] = useState([]);
+  const [attendanceObject, setAttendanceObject] = useState({});
+  const [showModal, setShowModal] = useState(false);
+  const [weekDays, setWeekDays] = useState([]);
+
+  useEffect(() => {
+    let ignore = false;
+    const getData = async () => {
+      if (!ignore) {
+        setWeekDays(calendar(weekPage, "month"));
+      }
+    };
+    getData();
+  }, [weekPage]);
+
   return (
     <Layout
       _header={{
@@ -46,6 +70,14 @@ const TeacherAttendanceReport = () => {
       }}
       _appBar={{ languages: ["en"] }}
     >
+      <Box p={5} bg={"gray.100"}>
+        <Text fontWeight={600} fontSize={16}>
+          Chandan Pandit
+        </Text>
+        <Text color={colors.date} fontSize={14}>
+          Class VI . Sec A
+        </Text>
+      </Box>
       <Box p={5} bg={"gray.100"}>
         <Collapsible
           defaultCollapse={true}
@@ -56,46 +88,12 @@ const TeacherAttendanceReport = () => {
           }
         >
           <Divider mb={4} />
-          <VStack space={6}>
-            <HStack alignItems="center" justifyContent="space-between">
-              <Text w={"20%"} fontSize={12}>
-                Jan 2022
-              </Text>
-              <Box w={"80%"} pr={3}>
-                <ProgressBar data={progressData} />
-              </Box>
-            </HStack>
-            <HStack alignItems="center" justifyContent="space-between">
-              <Text w={"20%"} fontSize={12}>
-                Dec 2021
-              </Text>
-              <Box w={"80%"} pr={3}>
-                <ProgressBar data={progressData} />
-              </Box>
-            </HStack>
-            <HStack alignItems="center" justifyContent="space-between">
-              <Text w={"20%"} fontSize={12}>
-                Nov 2021
-              </Text>
-              <Box w={"80%"} pr={3}>
-                <ProgressBar data={progressData} />
-              </Box>
-            </HStack>
-            <HStack alignItems="center" justifyContent="space-between">
-              <HStack alignItems="center">
-                <Box bg="#2BB639" w="15px" h="15px" rounded={4} />
-                <Text mx={2}>Present</Text>
-              </HStack>
-              <HStack alignItems="center">
-                <Box bg="#F57B7B" w="15px" h="15px" rounded={4} />
-                <Text mx={2}>Absent</Text>
-              </HStack>
-              <HStack alignItems="center">
-                <Box bg="#B5B5C8" w="15px" h="15px" rounded={4} />
-                <Text mx={2}>Unmarked</Text>
-              </HStack>
-            </HStack>
-          </VStack>
+          <CalendarComponent
+            monthDays={weekDays}
+            item={teacherObject}
+            attendance={attendance}
+            setAttendanceObject={setAttendanceObject}            
+          />
         </Collapsible>
       </Box>
       <Box px={5} bg={"gray.100"}>
@@ -152,6 +150,181 @@ const TeacherAttendanceReport = () => {
       </Box>
     </Layout>
   );
+};
+
+const CalendarComponent = ({
+  monthDays,
+  isIconSizeSmall,
+  attendance,
+  setAttendanceObject,
+  type,
+  item,
+  loding,
+  _weekBox,
+}) => {
+  return monthDays.map((week, index) => (
+    <HStack
+      key={index}
+      justifyContent="space-around"
+      alignItems="center"
+      p="0.5"
+      {...(_weekBox?.[index] ? _weekBox[index] : {})}
+    >
+      {week.map((day, subIndex) => {
+        let isToday = moment().format("Y-MM-DD") === day.format("Y-MM-DD");
+        let dateValue = day.format("Y-MM-DD");
+        let smsItem = attendance
+          .slice()
+          .reverse()
+          .find((e) => e.date === dateValue);
+        let smsIconProp = !isIconSizeSmall
+          ? {
+              _box: { py: 2, minW: "46px", alignItems: "center" },
+              status: "CheckboxBlankCircleLineIcon",
+            }
+          : {};
+        if (smsItem?.type && smsItem?.type === "Present") {
+          smsIconProp = {
+            ...smsIconProp,
+            status: smsItem?.type,
+            type: smsItem?.status,
+          };
+        } else if (smsItem?.type && smsItem?.type === "Absent") {
+          smsIconProp = {
+            ...smsIconProp,
+            status: smsItem?.type,
+            type: smsItem?.status,
+          };
+        } else if (smsItem?.type && smsItem?.type === "SpecialDuty") {
+          smsIconProp = {
+            ...smsIconProp,
+            status: smsItem?.type,
+            type: smsItem?.status,
+          };
+        } else if (day.day() === 0) {
+          smsIconProp = { ...smsIconProp, status: "Holiday" };
+        } else if (isToday) {
+          smsIconProp = { ...smsIconProp, status: "Today" };
+        } else if (moment().diff(day, "days") > 0) {
+          smsIconProp = { ...smsIconProp, status: "Unmarked" };
+        }
+
+        return (
+          <VStack
+            key={subIndex}
+            alignItems="center"
+            opacity={
+              type && type !== "month" && day.day() !== 0
+                ? 1
+                : day.day() === 0
+                ? 0.3
+                : day.format("M") !== moment().format("M")
+                ? 0.3
+                : 1
+            }
+          >
+            <Text
+              key={subIndex}
+              pt={monthDays.length > 1 && index ? 0 : !isIconSizeSmall ? 2 : 0}
+              textAlign="center"
+            >
+              {!isIconSizeSmall ? (
+                <VStack alignItems={"center"}>
+                  {index === 0 ? (
+                    <Text pb="4" color={colors.dateLight} fontSize={12}>
+                      {day.format("ddd")}
+                    </Text>
+                  ) : (
+                    ""
+                  )}
+                  <Text color={colors.date} fontSize={14} fontWeight={600}>
+                    {day.format("DD")}
+                  </Text>
+                </VStack>
+              ) : (
+                <HStack alignItems={"center"} space={1}>
+                  <Text>{day.format("dd")}</Text>
+                  <Text>{day.format("D")}</Text>
+                </HStack>
+              )}
+            </Text>
+            <TouchableHighlight onPress={(e) => setAttendanceObject(smsItem)}>
+              <Box alignItems="center">
+                {loding && loding[dateValue + item.id] ? (
+                  <GetIcon
+                    {...smsIconProp}
+                    status="Present"
+                    color={colors.primary}
+                    isDisabled
+                    _icon={{ _fontawesome: { spin: true } }}
+                  />
+                ) : (
+                  <GetIcon {...smsIconProp} status="SpecialDuty" />
+                )}
+              </Box>
+            </TouchableHighlight>
+          </VStack>
+        );
+      })}
+    </HStack>
+  ));
+};
+
+const GetIcon = ({ status, _box, color, _icon }) => {
+  let icon = <></>;
+  let iconProps = { fontSize: "xl", isDisabled: true, ..._icon };
+  switch (status) {
+    case "Present":
+      icon = (
+        <Box {..._box} color={color ? color : colors.attendancePresent}>
+          <IconByName name="CheckboxCircleLineIcon" {...iconProps} />
+        </Box>
+      );
+      break;
+    case "Absent":
+      icon = (
+        <Box {..._box} color={color ? color : colors.attendanceAbsent}>
+          <IconByName name="CloseCircleLineIcon" {...iconProps} />
+        </Box>
+      );
+      break;
+    case "SpecialDuty":
+      icon = (
+        <Box {..._box} color={color ? color : colors.specialDuty}>
+          <IconByName name="AwardLineIcon" {...iconProps} />
+        </Box>
+      );
+      break;
+    case "Holiday":
+      icon = (
+        <Box {..._box} color={color ? color : colors.holiDay}>
+          <IconByName name="CheckboxBlankCircleLineIcon" {...iconProps} />
+        </Box>
+      );
+      break;
+    case "Unmarked":
+      icon = (
+        <Box {..._box} color={color ? color : colors.attendanceUnmarked}>
+          <IconByName name="CheckboxBlankCircleLineIcon" {...iconProps} />
+        </Box>
+      );
+      break;
+    case "Today":
+      icon = (
+        <Box {..._box} color={color ? color : colors.attendanceUnmarked}>
+          <IconByName name="CheckboxBlankCircleLineIcon" {...iconProps} />
+        </Box>
+      );
+      break;
+    default:
+      icon = (
+        <Box {..._box} color={color ? color : colors.attendancedefault}>
+          <IconByName name={status} {...iconProps} />
+        </Box>
+      );
+      break;
+  }
+  return icon;
 };
 
 export default TeacherAttendanceReport;
